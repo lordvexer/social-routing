88066348e9b9a11c15764d49d74b15c879aecbab
HamidReza Mahmoudzadeh <h.nypd@yahoo.com>
2026-07-16 00:30:01 +0330

    212

--- worker.js
+++ worker.js
@@ -1,78 +1,10 @@
-export default {
+for (const service in data) {
 
-async fetch(request, env, ctx) {
+ if(service !== "meta")
+   continue;
 
-
-const source = env.GITHUB_JSON_URL;
-
-
-const response =
-await fetch(source);
-
-
-if(!response.ok){
-
-return new Response(
-"GitHub unavailable",
-{
-status:502
-}
-);
+ for(const cidr of data[service]) {
+   output += cidr + "|" + service + "\n";
+ }
 
 }
-
-
-
-const data =
-await response.json();
-
-
-
-let output = "";
-
-
-
-for(
-const service in data
-){
-
-
-for(
-const cidr of data[service]
-){
-
-
-output += cidr + "|" + service + "\n";
-
-
-}
-
-
-}
-
-
-
-return new Response(
-
-output,
-
-{
-
-headers:{
-
-"content-type":
-"text/plain",
-
-"cache-control":
-"public,max-age=21600"
-
-}
-
-}
-
-);
-
-
-}
-
-};
